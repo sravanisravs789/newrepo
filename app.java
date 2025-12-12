f027f4999b73592e86a84ca74bf7e07c0ca1ec33
sravani <sravani@flm.com>
2025-12-12 07:32:20 +0000

    code added by me

---
+++ app.java
@@ -0,0 +1,8 @@
+public class Car {
+  String brand = "Ford";
+  public static void main(String[] args) {
+    Car myCar = new Car(); // Create an object of the Car class
+    System.out.println(myCar.brand); // Print the value of the brand attribute
+  }
+}
+
